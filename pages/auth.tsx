379e4b6828cec2d6c43057cec09f7faef8fa0dc9
Your Name <you@example.com>
2023-05-23 17:31:47 +0200

    register function done

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useCallback, useState } from 'react';
 import Input from "@/components/Input";
 
@@ -12,6 +13,18 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register': 'login');
     },[]);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            });
+        }catch(error){
+            console.log(error);
+        }
+    }, [email, name, password]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -48,8 +61,8 @@ const Auth = () => {
                             value={password}
                         />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                            {variant === 'login' ? 'login': 'Sign up'}
+                        <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            {variant === 'login' ? 'Login': 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time using Netflix?': 'Already haver an account'}
