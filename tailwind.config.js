71ab83927a894fbeb56aa822b8ee30f274556a9b
Your Name <you@example.com>
2023-05-23 12:49:42 +0200

    front auth finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {
+      transform: ['peer-focus', 'peer-placeholder-shown'],
+      scale: ['peer-focus', 'peer-placeholder-shown'],
+      translate: ['peer-focus', 'peer-placeholder-shown'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
